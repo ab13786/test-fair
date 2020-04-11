0243fac1bbab903a2dd80b529800db4a0313d2cd
ab13786 <35544023+ab13786@users.noreply.github.com>
2020-04-11 13:29:03 -0400

    Add files via upload

--- web/views/Applications.php
+++ web/views/Applications.php
@@ -45,7 +45,42 @@
         </script>
 
         <div id="lego" class="tabcontent">
-            <button class="btn"><i class="fa fa-download"></i> Download</button>
+            <button name="download" class="btn" onclick=""><i class="fa fa-download"></i> Download</button>
+
+                <?PHP
+
+                if(array_key_exsists('download', $_POST){
+                      // Original PHP code by Chirp Internet: www.chirp.com.au
+                      // Please acknowledge use of this code by including this header.
+
+                      function cleanData(&$str)
+                      {
+                        $str = preg_replace("/\t/", "\\t", $str);
+                        $str = preg_replace("/\r?\n/", "\\n", $str);
+                        if(strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"';
+                      }
+
+                      // filename for download
+                      $filename = "website_data_" . date('Ymd') . ".xls";
+
+                      header("Content-Disposition: attachment; filename=\"$filename\"");
+                      header("Content-Type: application/vnd.ms-excel");
+
+                      $flag = false;
+                      $result = pg_query("SELECT * FROM table ORDER BY field") or die('Query failed!');
+                      while(false !== ($row = pg_fetch_assoc($result))) {
+                        if(!$flag) {
+                          // display field/column names as first row
+                          echo implode("\t", array_keys($row)) . "\r\n";
+                          $flag = true;
+                        }
+                        array_walk($row, __NAMESPACE__ . '\cleanData');
+                        echo implode("\t", array_values($row)) . "\r\n";
+                      }
+                      exit;
+                  }
+                ?>
+
             <section style="clear:both;">
                 <h1>Query for all lego applications in database</h1>
                 <?php
