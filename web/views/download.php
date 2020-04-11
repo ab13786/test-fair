2583157c44c8772cb623f928353c6e69bd37ecd6
ab13786 <35544023+ab13786@users.noreply.github.com>
2020-04-11 14:05:36 -0400

    Add files via upload

--- web/views/download.php
+++ web/views/download.php
@@ -12,7 +12,7 @@
         header("Content-Type: application/vnd.ms-excel");
         $user_query = "SELECT * From applications.LegoApp";
 
-        $flag = false;
+/*         $flag = false;
         while ($row = mysql_fetch_assoc($user_query)) {
             if (!$flag) {
                 // display field/column names as first row
@@ -20,5 +20,15 @@
                 $flag = true;
             }
             echo implode("\t", array_values($row)) . "\r\n";
+        } */
+
+        $results = $conn->query($user_query);
+
+        if($results->num_rows>0){
+            echo "<table><tr><th>LegoAppID</th><th>Builder 1 Name</th><th>Builder 2 Name</th><th>DOB Builder 1</th><th>Address</th><th>Phone Number</th><th>Email</th></tr>";
+            while($row = $results->fetch_assoc()){
+                echo "<tr><td>". $row['LegoAppID']. "</td><td>". $row['Builder1FN']. " ". $row['Builder1LN']. "</td><td>". $row['Builder2FN']. " ". $row['Builder2LN']. "</td><td>". $row['DOBuilder1']. "</td><td>". $row['Address']. "</td><td>". $row['PhoneNumber']. "</td><td>". $row['Email']. "</td></tr>";
+            }
+            echo "</table>";
         }
 ?>
