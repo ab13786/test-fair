430e099c3bd4082cc85f169ab55f0397e0b97523
ab13786 <35544023+ab13786@users.noreply.github.com>
2020-04-11 12:03:42 -0400

    Add files via upload

---
+++ web/views/getImage.php
@@ -0,0 +1,20 @@
+<?php
+                    $host = "ogeechee-fair.cyxvjubgt7cw.us-east-1.rds.amazonaws.com";
+                    $port = "3306";
+                    $user = "fair_admin";
+                    $password = "KiwanisClub";
+                    $db = "applications";
+
+                    $id = $_GET['id'];
+
+                    $con = new mysqli($host, $user, $password, $db);
+                    $sql = "SELECT Sponsors.SponsorLogo From applications.Sponsors WHERE Sponsors.SponsorID=$id";
+                    $results = $con->query($sql);
+
+                    $row = mysql_fetch_assoc($results);
+
+                    $con->close();
+
+                    header=("Content-type: image/jpeg");
+                    echo $row['SponsorLogo'];
+                ?>
