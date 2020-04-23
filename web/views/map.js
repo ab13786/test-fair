cbed95258cbcaf14f6e74bf860cb57ae82942370
ab13786 <35544023+ab13786@users.noreply.github.com>
2020-04-23 17:47:36 -0400

    Add files via upload

--- web/views/map.js
+++ web/views/map.js
@@ -9,7 +9,7 @@ function initMap() {
     //prompts the user to enter a name for the location.
     var name = prompt('Location name:');
     //this calls the function that add Marker to the map.
-    //addMarker({lat: 32.394424,lng:-81.752965}, new google.maps.Map(document.getElementById("map"), {zoom: 17, center: {lat: 32.395958,lng: -81.753546},  mapTypeId: 'satellite'}), "Entrance/Exit");
+    addMarker({lat: 32.394424,lng:-81.752965}, new google.maps.Map(document.getElementById("map"), {zoom: 17, center: {lat: 32.395958,lng: -81.753546},  mapTypeId: 'satellite'}), "Entrance/Exit");
     addMarker(e.latLng, map, name);
     });
 
