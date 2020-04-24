faac969232c1cfea598100ae0b524ca9a1f20cf1
ab13786 <35544023+ab13786@users.noreply.github.com>
2020-04-24 11:17:52 -0400

    Add files via upload

--- web/views/Vendors.php
+++ web/views/Vendors.php
@@ -23,7 +23,7 @@
         <div class="tab">
             <button class="tablinks" onclick="opentab(event,'allFood')">All Food</button>
             <button class="tablinks" onclick="opentab(event,'addFood')">Add Food</button>
-            <button class="tablinks" onclick="opentab(event,'AllRides')">All Rides</button>
+            <button class="tablinks" onclick="opentab(event,'allRides')">All Rides</button>
             <button class="tablinks" onclick="opentab(event,'addRides')">Add Rides</button>
         </div>
 
@@ -54,6 +54,32 @@
 
         <div id="allRides" class="tabcontent">
             <h1>Query for all Rides in database</h1>
+            <form action="" method="POST">
+                <label for = "del" id="none"> Delete Row: </label>
+                <input type="text" id="del" name="del" placeholder="Enter ID">
+
+                <input type="submit" value="Delete" id="delete" name="delete" class="deletion">
+            </form>
+            <?php
+                    $host = "ogeechee-fair.cyxvjubgt7cw.us-east-1.rds.amazonaws.com";
+                    $port = "3306";
+                    $user = "fair_admin";
+                    $password = "KiwanisClub";
+                    $db = "applications";
+
+                    $con = new mysqli($host, $user, $password, $db);
+                    $sql = "SELECT * From applications.rides";
+                    $results = $con->query($sql);
+
+                    if($results->num_rows>0){
+                        echo "<table><tr><th>rideID</th><th>Name</th><th>Description</th><th>Height Requirement</th><th>Tickets</th></tr>";
+                        while($row = $results->fetch_assoc()){
+                            echo "<tr class='notfirst'><td>". $row['ridesID']. "</td><td>". $row['name']. "</td><td>". $row['description']. "</td><td>". $row['requirements']. "</td><td>". $row['tickets']. "</td></tr>";
+                        }
+                        echo "</table>";
+                    }
+                    $con->close();
+                ?>
         </div>
 
         <div id="addRides" class="tabcontent">
