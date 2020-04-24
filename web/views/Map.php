49a1ae48dd68cf1f906759a97472c2f38a0a8d56
ab13786 <35544023+ab13786@users.noreply.github.com>
2020-04-24 15:26:15 -0400

    Add files via upload

--- web/views/Map.php
+++ web/views/Map.php
@@ -6,7 +6,6 @@
 
         <link rel="stylesheet" type="text/css" href="/stylesheets/map.css">
 
-        <script src="/views/map.js"></script>
     </head>
     <body>
         <header>
@@ -40,7 +39,116 @@
 
                     <!--script for map. -->
                     <script>
-                        initMap();
+
+// function to initiallize map.(
+function initMap() {
+    //location for the map to open. fair for center.
+    var fairLoc = {lat: 32.395958,lng: -81.753546};
+    //creates a google map with fair location.
+    var map = new google.maps.Map(document.getElementById("map"), {zoom: 17, center: fairLoc,  mapTypeId: 'satellite'});
+
+                    var title = [
+                <?php
+                    $host = "ogeechee-fair.cyxvjubgt7cw.us-east-1.rds.amazonaws.com";
+                    $port = "3306";
+                    $user = "fair_admin";
+                    $password = "KiwanisClub";
+                    $db = "applications";
+
+                    $con = new mysqli($host, $user, $password, $db);
+                    $sql = "SELECT * From applications.mapInfo;";
+                    $results = $con->query($sql);
+                    $name = "";
+
+                    if($results->num_rows>0){
+                        while($row = $results->fetch_assoc()){
+                            $name .= "'". $row['name']  . "',";
+                        }
+                    }
+                    echo $name;
+                    $con->close();
+                ?>
+            ''];
+
+            var spot = [
+                <?php
+                    $host = "ogeechee-fair.cyxvjubgt7cw.us-east-1.rds.amazonaws.com";
+                    $port = "3306";
+                    $user = "fair_admin";
+                    $password = "KiwanisClub";
+                    $db = "applications";
+
+                    $con = new mysqli($host, $user, $password, $db);
+                    $sql = "SELECT * From applications.mapInfo;";
+                    $results = $con->query($sql);
+                    $name = "";
+
+                    if($results->num_rows>0){
+                        while($row = $results->fetch_assoc()){
+                            $latLng.= "'". $row['latLng']  . "',";
+                        }
+                    }
+                    echo $latLng;
+                    $con->close();
+                ?>
+            ''];
+
+            for (var count = 0; count < title.length - 1; count++){
+                addMarker(title[count], map, spot[count]);
+            }
+    //gives the map a listener to when someone clicks to add a marker.
+    map.addListener('click', function(e) {
+    //prompts the user to enter a name for the location.
+    var name = prompt('Location name:');
+    //this calls the function that add Marker to the map.
+    addMarker(e.latLng, map, name);
+    });
+
+    //will need code to get position and name from database.
+    //addMarker(latLng, map, name); --this is the function you will need ones information is pulled.
+}
+
+//function to add markers to map.(latLng needs to be saved to the database with name)
+function addMarker(latLng, map, name) {
+
+    //places a marker with location fom click.
+    var marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    draggable: true
+});
+    //moves center of map to the new marker.
+    //map.panTo(latLng);
+    //adds an info window to the marker. gives the location a name.
+    marker.info = new google.maps.InfoWindow({
+    content: '<h2>' + name + '</h2>'
+    });
+
+    //this is an event listener for double clicking a marker.
+    google.maps.event.addListener(marker, 'dblclick', function() {
+    //prompts user to change name of marker. (this will need to be saved when changed.)
+    name = prompt('Location Name:')
+    marker.info.setContent('<h2>' + name + '</h2>');
+    });
+
+    //event listener to right click to delete marker.
+    google.maps.event.addListener(marker, 'rightclick', function() {
+    //if user says okay to prompt, marker will be deleted.
+    if(confirm("Do you want to delete marker?"))
+        marker.setMap(null);
+        //code to remove marker form database.
+    });
+
+    //mouseover marker info window pops up.
+    google.maps.event.addListener(marker, 'mouseover', function() {
+    marker.info.open(map, marker);
+    });
+
+    //moveout marker to close info window.
+    google.maps.event.addListener(marker, 'mouseout', function() {
+    marker.info.close(map, marker);
+    });
+}
                      </script>
 
                     <?php
